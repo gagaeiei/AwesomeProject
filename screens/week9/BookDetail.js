a650e1ec7bb8c66bb30f77e4847a8deafce03bfc
gagaeiei <160008396+gagaeiei@users.noreply.github.com>
2024-04-24 09:18:46 +0700

    f

--- screens/week9/BookDetail.js
+++ screens/week9/BookDetail.js
@@ -3,6 +3,7 @@ import { Alert, Image, Text, TouchableOpacity, View } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import BookStorage from "../../storages/BookStorage";
+import BookService from "../../services/BookService";
 
 export default function BookDetail() {
     
@@ -19,15 +20,20 @@ export default function BookDetail() {
     const deleteBook = async () => {
         const { id } = route.params;
         //REMOVE BOOK
-        await BookStorage.removeItem(id);
+        //await BookStorage.removeItem(id);
+        await BookService.destroyItem({"id":id});
         //REDIRECT TO
         navigation.navigate("Book");
       }; 
-      const onLoad = async () => {
-          navigation.setOptions({ headerRight: () => ( <TopRightMenu /> ) });              
-          const { id } = route.params;
-          let b = await BookStorage.readItemDetail(id);
-          setBook(b);
+      const onLoad = async () => {  
+        // navigation.setOptions({ headerRight: () => ( <TopRightMenu b={b} /> ) });                  
+        const { id } = route.params;
+        // let b = await BookStorage.readItemDetail(id);
+        let b = await BookService.getItemDetail({"id":id});
+        setBook(b);
+       
+        navigation.setOptions({ headerRight: () => ( <TopRightMenu b={b} /> ) });
+
       };
       useEffect(() => { onLoad(); }, []);
  
